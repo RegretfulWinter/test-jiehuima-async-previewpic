9b25cbf081a52b6682e3317930e8194983e82225
Claudia Ma <awfultangerine@gmail.com>
2024-03-07 17:21:26 -0800

    test deploy

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,13 @@ export default function Home() {
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
+        <head>
+    <title>Hello world</title>
+    <meta
+      property="og:image"
+      content="https://og-examples.vercel.sh/api/static"
+    />
+</head>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
